b7950a9f7ecfbd324b2aff65f00494b31697b552
Azizbek Khalilov <56731393+TDarkCoder@users.noreply.github.com>
2020-03-26 17:03:05 +0500

    Database configuration edited

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 
     defined('host') ? null : define('host', 'localhost');
-    defined('user') ? null : define('user', 'gallery');
-    defined('password') ? null : define('password', 'coderKing');
+    defined('user') ? null : define('user', 'root');
+    defined('password') ? null : define('password', '');
     defined('db') ? null : define('db', 'photo_gallery');
